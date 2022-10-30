ab4fa7d53356b7581819d789ddb7e2436b5a965f
su-sid <davidsudi20@gmail.com>
2022-10-30 16:32:42 +0300

    first commit

---
+++ hngi9-Task1/index.js
@@ -0,0 +1,29 @@
+//.src/index.js
+const express= require('express')
+
+//defining the express app
+const app=express()
+
+
+// use the express-static middleware
+app.use(express.static("/"))
+
+// defining an object
+const ads = { slackUsername:"su-sid",
+              backend:true,
+              age:22,
+              bio:'Free thinker, ready to network with other people'
+            }
+
+//converting the object to a json object
+const adsJSON=JSON.stringify(ads)
+
+//defining an endpoint to return all data
+app.get('/', (req, res) => {
+    res.send(ads);
+  });
+
+  //starting the server
+  app.listen(process.env.PORT ||5000,()=>{
+                        console.log(`listening on the open port `)
+                      })
